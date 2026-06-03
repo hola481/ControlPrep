c89382ab33e2ff3fbab1010df6c6898bb4869192
hola481 <hola@controlprep.com>
2026-06-03 13:59:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,18 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+const CACHE_NAME = 'controlprep-v3';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => caches.delete(key)));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Network only - no caching
+  event.respondWith(fetch(event.request));
+});
